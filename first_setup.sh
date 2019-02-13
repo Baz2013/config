69c3aedc729d68bb9ceea17c19243160c46bed6f
Baz2013 <chuan.gu6@gmail.com>
2019-02-13 20:52:22 +0800

    修改setup文件

--- first_setup.sh
+++ first_setup.sh
@@ -2,7 +2,11 @@
 
 #装机必备设置和必备软件
 
+# 升级系统
+apt-get update && apt-get -y upgrade
+
 # 设置语言和时区
 apt-get -y install language-pack-zh-hans
 export LC_ALL=zh_CN.UTF-8
 echo 'LANG="zh_CN.UTF-8"' > /etc/default/locale
+cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
